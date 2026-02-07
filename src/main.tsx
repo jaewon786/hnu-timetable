eae7905ea02c760a5bf446c494b23c018839c890
jaewon <jaewono786@gmail.com>
2026-02-07 16:52:18 +0900

    feat: Vercel Analytics 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.tsx'
 import ErrorBoundary from './components/ErrorBoundary.tsx'
 import './index.css'
@@ -6,5 +7,6 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
   <ErrorBoundary>
     <App />
+    <Analytics />
   </ErrorBoundary>
 )
